f0aa16ebe498505e1275e31dbcb2c0caaaeb9eb3
Ross Butler <github@rwbutler.com>
2019-07-02 11:53:21 +0100

    Updated tap for TypographyKit Palette

--- palette.rb
+++ palette.rb
@@ -1,9 +1,9 @@
 class Palette < Formula
   desc "Makes your TypographyKit color palette available in Xcode Interface Builder."
   homepage "https://github.com/rwbutler/TypographyKitPalette"
-  version "1.0.0"
+  version "1.1.0"
   url "https://github.com/rwbutler/TypographyKitPalette/releases/download/#{version}/palette"
-  sha256 "fbceb7134b1444d2a27b91ae030eac3ccbc4e93eba1a67455b1c65612458d12f"
+  sha256 "e35e23ef25c046a7fa8ae5e7ec55616c052e553757ffb65c9f6573ec914efeb1"
 def install
     bin.install "palette"
   end
