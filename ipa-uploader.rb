572d856d7acdbd283749d91d77855047028cc975
Ross Butler <github@rwbutler.com>
2019-09-24 11:38:39 +0100

    IPAUploader -> 1.1.3

--- ipa-uploader.rb
+++ ipa-uploader.rb
@@ -1,9 +1,9 @@
 class IpaUploader < Formula
   desc "Uploads your apps to TestFlight & App Store."
   homepage "https://github.com/rwbutler/IPAUploader"
-  version "1.1.2"
+  version "1.1.3"
   url "https://github.com/rwbutler/IPAUploader/releases/download/#{version}/ipa-uploader"
-  sha256 "7fadf008e3387bd63094bc7b8ce948738f5dea1509bdf8c93ec3094d331f7fda"
+  sha256 "edc3ea0373c771ca3cf5ad127dbcea924d88aa18502e2d9442385446e9abd160"
 def install
     bin.install "ipa-uploader"
   end
