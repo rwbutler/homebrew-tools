9b195863aa82d4b504ea741dc09ef1b7e61ad97f
Ross Butler <github@rwbutler.com>
2020-04-20 21:17:41 +0100

    ConfigValidator -> 1.1.0

---
+++ config-validator.rb
@@ -0,0 +1,18 @@
+class ConfigValidator < Formula
+  desc "Config Validator validates & uploads your configuration files and cache clears your CDN as part of your CI process."
+  homepage "https://github.com/rwbutler/ConfigValidator"
+  version "1.1.0"
+  url "https://github.com/rwbutler/ConfigValidator.git",
+  :tag => "#{version}"
+  head "https://github.com/rwbutler/ConfigValidator.git"
+  
+  depends_on :xcode => ["11.0", :build]
+  
+  def install
+    system "make", "install", "prefix=#{prefix}"
+  end
+
+  test do
+    system "config-validator" "--help"
+  end
+end
