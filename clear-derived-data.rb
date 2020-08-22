08042474e50a7f093e23bc1c2f3cc22cbf1a525c
Ross Butler <github@rwbutler.com>
2020-08-22 21:23:41 +0100

    ClearDerivedData 1.0.0 -> 1.0.1

--- clear-derived-data.rb
+++ clear-derived-data.rb
@@ -1,9 +1,9 @@
 class ClearDerivedData < Formula
   desc "Script for quickly emptying the DerivedData directory."
   homepage "https://github.com/rwbutler/ClearDerivedData"
-  version "1.0.0"
+  version "1.0.1"
   url "https://github.com/rwbutler/ClearDerivedData.git",
-  :tag => "#{version}", :revision => "241aa573dc8c8c43206c728cff71e9656741c028"
+  :tag => "#{version}", :revision => "8686f74629053ef3eaebc91078dc206d85531136"
   head "https://github.com/rwbutler/ClearDerivedData.git"
   
   depends_on :xcode => ["11.0", :build]
