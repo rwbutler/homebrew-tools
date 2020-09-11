41ea0100ccdabeb8242774a6a45b9dd9efbb516b
Ross Butler <github@rwbutler.com>
2020-09-11 12:25:21 +0100

    QSH 0.0.4 -> 0.0.5

--- qsh.rb
+++ qsh.rb
@@ -1,9 +1,9 @@
 class Qsh < Formula
   desc "Interactive quiz shell for macOS Terminal."
   homepage "https://github.com/rwbutler/QSH"
-  version "0.0.4"
+  version "0.0.5"
   url "https://github.com/rwbutler/homebrew-tools/raw/master/qsh"
-  sha256 "0116799e181d72e49b6307321e633ef7057d1b4f61f8ea594ee671b219284579"
+  sha256 "42d52572bd8ce12d7855ef2ce99551b8bc04e8fa61dfb402dfb3dcb00d7dc304"
 def install
     bin.install "qsh"
   end
